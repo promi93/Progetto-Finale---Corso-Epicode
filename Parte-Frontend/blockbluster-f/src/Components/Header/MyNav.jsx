94b9e555c6fc2555ce49e63c2989005c5728af8e
promi93 <116350522+promi93@users.noreply.github.com>
2023-07-13 22:40:26 +0200

    Revert "x"
    This reverts commit 865d80f846d75ecf348dbed94a12e27a9bfd823e.

--- Parte-Frontend/blockbluster-f/src/Components/Header/MyNav.jsx
+++ Parte-Frontend/blockbluster-f/src/Components/Header/MyNav.jsx
@@ -1,25 +1,17 @@
-import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { BsSearch } from "react-icons/bs";
-import { FaUser } from "react-icons/fa";
 
 function NavBar() {
-  const [showSearch, setShowSearch] = useState(false);
-
-  const handleSearchToggle = () => {
-    setShowSearch(!showSearch);
-  };
-
   return (
-    <Navbar expand="lg" className="bg-body-tertiary bg-nav shadow">
+    <Navbar expand="lg" className="bg-body-tertiary">
       <Container fluid>
         <Navbar.Brand href="#">BlockBluster</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll" className="me-4">
+        <Navbar.Collapse id="navbarScroll">
           <Nav
             className="me-auto my-2 my-lg-0 ms-5 ps-5"
             style={{ maxHeight: "100px" }}
@@ -30,24 +22,17 @@ function NavBar() {
             <Nav.Link href="#action3">About us</Nav.Link>
           </Nav>
           <Form className="d-flex">
-            {showSearch && (
-              <Form.Control
-                type="search"
-                placeholder="cerca..."
-                className="me-2"
-                aria-label="Search"
-                id="outlineBtn"
-              />
-            )}
-            <Button
-              className={`search-btn ${showSearch ? "hidden" : ""}`}
-              variant="transparent"
-              onClick={handleSearchToggle}
-            >
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+              id="btn-search"
+            />
+            <Button className="btn" variant="trasparent">
               <BsSearch />
             </Button>
           </Form>
-          <FaUser className="fa" />
         </Navbar.Collapse>
       </Container>
     </Navbar>
