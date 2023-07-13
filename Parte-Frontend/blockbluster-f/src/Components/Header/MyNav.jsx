865d80f846d75ecf348dbed94a12e27a9bfd823e
promi93 <116350522+promi93@users.noreply.github.com>
2023-07-13 22:27:30 +0200

    x

--- Parte-Frontend/blockbluster-f/src/Components/Header/MyNav.jsx
+++ Parte-Frontend/blockbluster-f/src/Components/Header/MyNav.jsx
@@ -1,17 +1,25 @@
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { BsSearch } from "react-icons/bs";
+import { FaUser } from "react-icons/fa";
 
 function NavBar() {
+  const [showSearch, setShowSearch] = useState(false);
+
+  const handleSearchToggle = () => {
+    setShowSearch(!showSearch);
+  };
+
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className="bg-body-tertiary bg-nav shadow">
       <Container fluid>
         <Navbar.Brand href="#">BlockBluster</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
+        <Navbar.Collapse id="navbarScroll" className="me-4">
           <Nav
             className="me-auto my-2 my-lg-0 ms-5 ps-5"
             style={{ maxHeight: "100px" }}
@@ -22,17 +30,24 @@ function NavBar() {
             <Nav.Link href="#action3">About us</Nav.Link>
           </Nav>
           <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-              id="btn-search"
-            />
-            <Button className="btn" variant="trasparent">
+            {showSearch && (
+              <Form.Control
+                type="search"
+                placeholder="cerca..."
+                className="me-2"
+                aria-label="Search"
+                id="outlineBtn"
+              />
+            )}
+            <Button
+              className={`search-btn ${showSearch ? "hidden" : ""}`}
+              variant="transparent"
+              onClick={handleSearchToggle}
+            >
               <BsSearch />
             </Button>
           </Form>
+          <FaUser className="fa" />
         </Navbar.Collapse>
       </Container>
     </Navbar>
