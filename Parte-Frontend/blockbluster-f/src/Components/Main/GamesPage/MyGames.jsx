04f9ce05cb58f13bab76146b71448b3cd9e30716
promi93 <116350522+promi93@users.noreply.github.com>
2023-07-19 11:36:17 +0200

    rating ok

--- Parte-Frontend/blockbluster-f/src/Components/Main/GamesPage/MyGames.jsx
+++ Parte-Frontend/blockbluster-f/src/Components/Main/GamesPage/MyGames.jsx
@@ -2,9 +2,12 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import Spinner from "react-bootstrap/Spinner";
+import { BsCart4, BsInfoCircleFill } from "react-icons/bs";
 
 function MyGames() {
   const [games, setGames] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch("http://localhost:8080/api/auth/games", {
@@ -20,21 +23,31 @@ function MyGames() {
           throw new Error("Errore durante il recupero dei giochi");
         }
       })
-      .then((data) => setGames(data))
-      .catch((error) =>
-        console.error("Errore durante il recupero dei giochi:", error)
-      );
+      .then((data) => {
+        setGames(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Errore durante il recupero dei giochi:", error);
+        setLoading(false);
+      });
   }, []);
 
   if (games.length === 0) {
-    return <div>Loading...</div>;
+    return (
+      <div className="d-flex justify-content-center mt-5">
+        <Spinner animation="border" role="status" className="text-secondary">
+          <span className="sr-only"></span>
+        </Spinner>
+      </div>
+    );
   }
 
   return (
     <div className="d-flex flex-wrap justify-content-center mt-5">
       {games.map((game) => (
-        <Card className="card">
-          <Link key={game.id} to={`/games/${game.id}`} className="card-link">
+        <Card className="card" key={game.id}>
+          <Link to={`/games/${game.id}`} className="card-link">
             <Card.Img src={game.image} style={{ height: "200px" }} />
           </Link>
           <Card.Body>
@@ -52,8 +65,13 @@ function MyGames() {
               </span>
             </Card.Text>
             <Button className="nolo-btn" variant="transparent">
-              Noleggia
+              <BsCart4 />
             </Button>
+            <Link to={`/games/${game.id}`} className="card-link">
+              <Button className="info-btn ms-5" variant="transparent">
+                <BsInfoCircleFill />
+              </Button>
+            </Link>
           </Card.Body>
         </Card>
       ))}
--- Parte-Frontend/blockbluster-f/src/Components/Main/GamesPage/SingleCard.jsx
+++ Parte-Frontend/blockbluster-f/src/Components/Main/GamesPage/SingleCard.jsx
@@ -2,10 +2,15 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import Spinner from "react-bootstrap/Spinner";
 
 function SingleCard() {
   const { id } = useParams();
   const [gameData, setGameData] = useState(null);
+  const [rating, setRating] = useState(() => {
+    const savedRating = localStorage.getItem(`rating-${id}`);
+    return savedRating ? parseInt(savedRating, 10) : 0;
+  });
 
   useEffect(() => {
     fetch(`http://localhost:8080/api/auth/games/${id}`, {
@@ -27,8 +32,22 @@ function SingleCard() {
       );
   }, [id]);
 
+  useEffect(() => {
+    localStorage.setItem(`rating-${id}`, rating.toString());
+  }, [id, rating]);
+
+  const handleRating = (value) => {
+    setRating(value);
+  };
+
   if (!gameData) {
-    return <div>Loading...</div>;
+    return (
+      <div className="d-flex justify-content-center mt-5">
+        <Spinner animation="border" role="status" className="text-secondary">
+          <span className="sr-only"></span>
+        </Spinner>
+      </div>
+    );
   }
 
   return (
@@ -49,6 +68,25 @@ function SingleCard() {
               <p> {gameData.gamePrice}€ </p>
               <span className="availability text-info">Disponibilità:</span>
               <p> {gameData.isAvailable ? "Yes" : "No"} </p>
+              <span className="rating text-info">Valutazione:</span>
+              <div className="stella">
+                {[1, 2, 3, 4, 5].map((value) => (
+                  <svg
+                    key={value}
+                    onClick={() => handleRating(value)}
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill={value <= rating ? "gold" : "none"}
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="feather feather-star"
+                  >
+                    <polygon points="12 2 15.09 8.32 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.32 12 2" />
+                  </svg>
+                ))}
+              </div>
             </Card.Text>
             <Button className="nolo-btn" variant="transparent">
               Noleggia
